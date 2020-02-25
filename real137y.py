e9800c9f8517421a9f4e048a138642ef8b7e1d40
dnsata22 <60886113+dnsata22@users.noreply.github.com>
2020-02-25 08:44:37 -0500

    hangman

---
+++ real137y.py
@@ -0,0 +1,12 @@
+def test(): 
+    global letter
+    letter=int(input(What is letter :))
+    if letter ub secret:
+        pos=secret.find(letter)
+        updatedWord[pos]
+        ifWon()
+        
+    else:
+        updated(updatedWord)
+        global updatedWord
+    
